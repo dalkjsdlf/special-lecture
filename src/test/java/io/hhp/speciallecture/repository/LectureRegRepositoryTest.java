684d9fab3f827be8737e738fa510f74fa3c535bb
dalkjsdlf <dalkjsdlf@gmail.com>
2024-03-25 23:32:57 +0900

    레파지토리 어노테이션만 작성

--- src/test/java/io/hhp/speciallecture/repository/LectureRegRepositoryTest.java
+++ src/test/java/io/hhp/speciallecture/repository/LectureRegRepositoryTest.java
@@ -1,5 +1,10 @@
 package io.hhp.speciallecture.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DisplayName("레파지토리 테스트 - 아직 미완")
+@DataJpaTest
 public class LectureRegRepositoryTest {
 
 }
